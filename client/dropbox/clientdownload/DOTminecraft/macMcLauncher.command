12b27a70810374e9a173542a55a14add0912725e
berendbro <44678686+berendbro@users.noreply.github.com>
2019-05-09 21:49:40 +0200

    first file import
    i placed my entire "enigmaticookie file in here, the server folder now has the server file download (needs to be prepared for AMP use + add our mods and stuff). the client folder has the old client download (to be converted to work with the new server) also has the client download (from jonas cuz curseforge one sucks) and the forge installer jar (also from jonas cuz it's from the future)

---
+++ client/dropbox/clientdownload/DOTminecraft/macMcLauncher.command
@@ -0,0 +1,2 @@
+java -Xms1G -Xmx1G -jar serversync-2.6.17.jar silent
+open -a Minecraft
---
+++ client/dropbox/clientdownload/DOTminecraft/sab.bat
@@ -0,0 +1,2 @@
+java -Xms2G -Xmx2G -jar serversync-2.6.17.jar progress-only
+start "minecraft" "C:\Program Files (x86)\Minecraft\MinecraftLauncher.exe"
